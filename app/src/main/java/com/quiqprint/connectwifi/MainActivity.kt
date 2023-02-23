34cfd28a3b570c6ecc24e3cd85be7a7e777ea49c
manjit <dhaliwal20825@gmail.com>
2023-02-23 13:07:23 +0530

    commit

--- app/src/main/java/com/quiqprint/connectwifi/MainActivity.kt
+++ app/src/main/java/com/quiqprint/connectwifi/MainActivity.kt
@@ -6,16 +6,12 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.net.Network
-import android.net.NetworkCapabilities
-import android.net.NetworkRequest
 import android.net.wifi.ScanResult
 import android.net.wifi.WifiManager
-import android.net.wifi.WifiNetworkSpecifier
 import android.net.wifi.WifiNetworkSuggestion
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -25,7 +21,7 @@ import com.thanosfisherman.wifiutils.wifiScan.ScanResultsListener
 import com.thanosfisherman.wifiutils.wifiState.WifiStateListener
 
 @RequiresApi(Build.VERSION_CODES.Q)
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), WifiStateListener, ScanResultsListener {
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding
@@ -41,9 +37,7 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
-        connectivityManager =
-            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
+        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
 
         binding?.btnConnect?.setOnClickListener {
             ssid = binding?.evSSD?.text.toString()
@@ -53,49 +47,65 @@ class MainActivity : AppCompatActivity() {
             } else if (password.isNullOrEmpty()) {
                 Toast.makeText(this, "Both fields must not be empty", Toast.LENGTH_SHORT).show()
             } else {
-                connectToWifi()
+                connectWIFI()
             }
         }
     }
 
 
-    private fun connectToWifi() {
-        val specifier = WifiNetworkSpecifier.Builder()
-            .setSsid(ssid!!)
-            .setWpa2Passphrase(password!!)
-            .build()
+    private fun connectWIFI() {
+        val suggestion1 = password?.let {
+            WifiNetworkSuggestion.Builder()
+                .setSsid(ssid!!)
+                .setWpa2Passphrase(it)
+                .setIsAppInteractionRequired(true) // Optional (Needs location permission)
+                .build()
+        }
 
-        val request = NetworkRequest.Builder()
-            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-            .setNetworkSpecifier(specifier)
-            .build()
+        val suggestionsList = listOf(suggestion1)
 
-        val networkCallback = object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                super.onAvailable(network)
-                wifiNetwork = network
+        wifiManager.removeNetworkSuggestions(suggestionsList)
+        val status = wifiManager.addNetworkSuggestions(suggestionsList)
+
+        if (status != WifiManager.STATUS_NETWORK_SUGGESTIONS_SUCCESS) {
+            // do error handling here
+        }
+
+
+        val panelIntent = Intent(Settings.ACTION_WIFI_SETTINGS)
+        startActivityForResult(panelIntent, 0)
+//            WifiUtils.withContext(applicationContext).disableWifi()
+//            WifiUtils.withContext(applicationContext).enableWifi(this)
+    }
+
+    override fun isSuccess(isSuccess: Boolean) {
+        WifiUtils.withContext(applicationContext).scanWifi(this).start()
+    }
+
+    override fun onScanResults(scanResults: MutableList<ScanResult>) {}
+
+    // Optional (Wait for post connection broadcast to one of your suggestions)
+    private val intentFilter =
+        IntentFilter(WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION);
+
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (!intent.action.equals(WifiManager.ACTION_WIFI_NETWORK_SUGGESTION_POST_CONNECTION)) {
+                return
             }
-        }
 
-        connectivityManager.requestNetwork(request, networkCallback)
-    }
-
-    private fun disconnectFromWifi() {
-        if (wifiNetwork != null) {
-            connectivityManager.unregisterNetworkCallback(networkCallback)
-            wifiNetwork = null
+            Toast.makeText(applicationContext, "Connected", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
-        override fun onLost(network: Network) {
-            super.onLost(network)
-            wifiNetwork = null
-        }
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(broadcastReceiver, intentFilter)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        disconnectFromWifi()
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(broadcastReceiver)
     }
+
 }
